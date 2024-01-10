35637dc0743cdc11534da2daab40a559a5673ea0
williamqsntdev <quartz_09kudzus@icloud.com>
2024-01-10 22:07:23 +0100

    ok

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -59,6 +59,7 @@ const CourseIdPage = async ({
     course.title,
     course.description,
     course.imageUrl,
+    course.price,
     course.categoryId,
     course.chapters.some(chapter => chapter.isPublished),
   ];
